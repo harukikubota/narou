bc9eb015855e8838143866e2562932fb877cb5b0
HarukiKubota <ricca545@icloud.com>
2020-06-03 21:11:53 +0900

    develop-0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Narou.MixProject do
   def project do
     [
       app: :narou,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [espec: :test],
@@ -20,9 +20,8 @@ defmodule Narou.MixProject do
 
   defp deps do
     [
-      {:distillery, "~> 2.0"},
       {:espec, "~> 1.8.2", only: :test},
-      {:httpoison, "~>0.11"},
+      {:httpoison, "~> 1.6"},
       {:poison, "~> 4.0"},
       {:vex, "~> 0.8"}
     ]

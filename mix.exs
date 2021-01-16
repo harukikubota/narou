30acc21dd9f445a3a74775e96aaa243440935bfc
HarukiKubota <hk15304009@ga.tera-house.ac.jp>
2021-01-16 22:16:32 +0900

    Revert "Develop 0.2.6"

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Narou.MixProject do
   def project do
     [
       app: :narou,
-      version: "0.2.6",
-      elixir: "~> 1.11",
+      version: "0.2.4",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [espec: :test],
       deps: deps()
